d0e047d931a6e4a0620aa23aa3152497ee58a722
xuanhai <haivx0206@gmail.com>
2020-04-09 23:24:25 +0700

    add build flow

--- frontend/build-utils/webpack.development.js
+++ frontend/build-utils/webpack.development.js
@@ -4,14 +4,6 @@ module.exports = () => ({
         {
           test: /\.(css|scss)$/,
           use: ["style-loader", "css-loader",  "postcss-loader", "sass-loader"]
-        }, 
-        {
-          test: /\.(png|jpg|jpeg|gif)$/i,
-          use: [
-            {
-              loader: 'file-loader',
-            },
-          ],
         },
       ]
     }
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -4,46 +4,59 @@ const Htmlwebpackplugins = require("html-webpack-plugin");
 const tailwindcss = require("tailwindcss");
 const path = require("path");
 
-const envConfig = env => require(`./build-utils/webpack.${env.mode}.js`)(env);
+const modeConfig = (mode) => require(`./build-utils/webpack.${mode}`)(mode);
+const presetConfig = require("./build-utils/loadPresets");
 
-module.exports = env => {
+module.exports = ({ mode = "production", presets = [] }) => {
     return webpackMerge(
         {
             entry: "./index.js",
             output: {
                 path: __dirname + "/dist",
-                filename: "bundle.js"
+                filename: "bundle.js",
             },
-            mode: env.mode,
+            mode: mode,
             module: {
                 rules: [
                     {
                         test: /\.(js|jsx)$/i,
                         exclude: /node_modules/,
                         use: {
-                            loader: "babel-loader"
-                        }
-                    }
-                ]
+                            loader: "babel-loader",
+                        },
+                    },
+                    {
+                        test: /\.(png|jpg|jpeg|gif)$/i,
+                        use: [
+                            {
+                                loader: "url-loader",
+                                options: {
+                                    limit: 2500,
+                                },
+                            },
+                        ],
+                    },
+                ],
             },
             devServer: {
-                historyApiFallback: true
+                historyApiFallback: true,
             },
             plugins: [
                 new webpack.ProgressPlugin(),
                 new Htmlwebpackplugins({
                     template: "./public/index.html",
-                    filename: "./index.html"
-                })
+                    filename: "./index.html",
+                }),
             ],
             resolve: {
                 alias: {
                     Components: path.resolve(__dirname, "src/components/"),
                     Containers: path.resolve(__dirname, "src/containers"),
-                    Styles: path.resolve(__dirname, "src/styles")
-                }
-            }
+                    Styles: path.resolve(__dirname, "src/styles"),
+                },
+            },
         },
-        envConfig(env)
+        modeConfig(mode),
+        presetConfig({ mode, presets })
     );
 };
