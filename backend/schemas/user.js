57e5c8b717e47e199f6130f89543a53e6f7179ba
xuanhai <haivx0206@gmail.com>
2020-04-26 11:39:10 +0700

    add backend

--- backend/schemas/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const usersSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String,
-});
-
-module.exports = usersSchema;
